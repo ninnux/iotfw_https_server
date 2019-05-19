8cdbd7678134737ba764c8bedb99fddd0433b13b
Nino Ciurleo <nino@ninux.org>
2019-05-19 10:24:22 -0800

    log print

--- https.go
+++ https.go
@@ -6,6 +6,7 @@ import (
     "net/http"
     "log"
     "strings"
+    "time"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
@@ -31,26 +32,24 @@ func HelloServerRoot(w http.ResponseWriter, req *http.Request) {
     // fmt.Fprintf(w, "This is an example server.\n")
     // io.WriteString(w, "This is an example server.\n")
     //fmt.Println(req)
-    fmt.Println(req.URL)
+    //fmt.Println(req.URL)
+    t:=time.Now()
+    fmt.Print(t.Format(time.UnixDate))
+    fmt.Print(",")
+    fmt.Print(req.RemoteAddr)
+    fmt.Print(",")
     fmt.Println(req.RequestURI)
     getparameters:=strings.Split(req.RequestURI,"/")
+    //fmt.Println(getparameters)
     if(len(getparameters)==3){
-    	fmt.Println(getparameters)
     	project_name:=getparameters[1]
     	fw_version:=getparameters[2]
     	url:="https://iotfw.ninux.org/"
-    	//for i, v := range getparameters{
-    	//    if i !=0 {
-    	//    	fmt.Println(v)
-    	//		//fmt.Fprintf(w, "%s ",v)
-    	//    }
-    	//}
-    	//fmt.Fprintf(w, "\n")
-    	//fmt.Fprintf(w, "%s%s_%s_%s.bin\n",url,getparameters[1],getparameters[2],getparameters[3])
-    	//fmt.Fprintf(w, "1,%sfw/blablablabla/culo/fw/vediamochesuccedeselurldiventamoltopiulungo/pippo.bin\n",url)
 	switch {
 	//case project_name=="https_server" && fw_version=="0.4":
     	//	fmt.Fprintf(w, "1,%sfw/iperf0.1.bin,",url)
+	case project_name=="https_server" && fw_version=="CasettaCaplsule2.4_0.5":
+    	 	fmt.Fprintf(w, "1,%sfw/https_server_CasettaCaplsule2.4_0.6.bin,",url)
 	case project_name=="esp-idf_wifi_mqtt_bme280" && fw_version=="0.1":
     	 	fmt.Fprintf(w, "1,%sfw/https_server0.4.bin,",url)
 	case project_name=="https_server" && fw_version=="0.4":
